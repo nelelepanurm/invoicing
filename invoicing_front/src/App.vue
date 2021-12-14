4b593bb87dcdf8ac87ca7c125eac57230f3b456e
Nadezda <nadezda.horeva@heathmont.net>
2021-12-14 17:01:42 +0200

    App vue 13.12. leong täiendus, Invoices back

--- invoicing_front/src/App.vue
+++ invoicing_front/src/App.vue
@@ -3,6 +3,7 @@
     <v-navigation-drawer
         v-model="drawer"
         app
+        v-if="token"
     >
       <v-list>
         <v-list-item
@@ -14,20 +15,11 @@
           <v-list-item-icon>
             <v-icon>{{ item.icon }}</v-icon>
           </v-list-item-icon>
-
           <v-list-item-content>
             <v-list-item-title>{{ item.title }}</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
       </v-list>
-
-      <template v-slot:append>
-        <div class="pa-2">
-          <v-btn block>
-            Logout
-          </v-btn>
-        </div>
-      </template>
     </v-navigation-drawer>
 
     <v-app-bar
@@ -36,22 +28,39 @@
         dark
     >
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-
       <v-toolbar-title>Invoicing</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <div v-if="token">
+        <v-btn v-on:click="logOut">Log out</v-btn>
+      </div>
     </v-app-bar>
 
     <v-main>
+      <v-container>
+        <div v-if="!token" class="home pa-10">
+          <h1>Lets get started!</h1>
+          <p class="font-weight-bold">
+            Welcome to Invoicing page!<br>
+            To start using Invoicing, please log in here: <br><br>
+          <div>
+            Kasutajanimi: <input v-model="newuser.userName" placeholder="kasutaja nimi"><br><br>
+            Parool: <input v-model="newuser.password" type="password" placeholder="parool"><br><br>
+            <v-btn v-on:click="login">LOGIN</v-btn>
+            <br><br>
+            <v-btn v-on:click="getData">Get data</v-btn>
+          </div>
+          <br><br>
+          <p class="font-weight-bold"> Don't have an account yet? <br> Register
+            <v-btn @click="registerHere()">HERE</v-btn>
+          </p>
+        </div>
+      </v-container>
       <div v-if="token">
         <router-view/>
-        <v-btn v-on:click="logOut">Log out</v-btn>
+
       </div>
       <div v-if="!token">
-        <v-container>
-          Kasutajanimi: <input v-model="newuser.userName">
-          Parool: <input v-model="newuser.password" type="password">
-          <v-btn v-on:click="login">Login</v-btn>
-          <v-btn v-on:click="getData">Get data</v-btn>
-        </v-container>
+
       </div>
     </v-main>
 
@@ -60,22 +69,22 @@
 </template>
 
 
-
 <script>
+import router from "./router";
+
 export default {
   data: () => ({
     drawer: null,
     items: [
-      {title: 'Home', icon: 'mdi-home', to: '/'},
-      {title: 'My Profile', icon: 'mdi-account-cog-outline', to: '/MyProfile'},
+      {title: 'Home', icon: 'mdi-home', to: '/Home2'},
       {title: 'Invoices', icon: 'mdi-receipt', to: '/Invoices'},
       {title: 'Create Invoice', icon: 'mdi-file-document-edit-outline', to: '/CreateInvoice'},
       {title: 'Clients', icon: 'mdi-account-group', to: '/Clients'},
       {title: 'VAT Codes', icon: 'mdi-format-list-group', to: '/Vat'},
-
+      {title: 'My Profile', icon: 'mdi-account-cog-outline', to: '/MyProfile'},
     ],
     token: '',
-    newuser:{}
+    newuser: {}
 
   }),
   methods: {
@@ -85,7 +94,9 @@ export default {
             this.token = result.data
             localStorage.setItem('user-token', this.token)
             this.$http.defaults.headers.common['Authorization'] = "Bearer " + this.token
+            router.push({name: 'Home2'})
           });
+
     },
     getData() {
       this.$http.get('api/protected')
@@ -102,6 +113,9 @@ export default {
       alert("you have been logged out")
       location.reload()
     }
+  },
+  mounted() {
+    this.token = localStorage.getItem("user-token")
   }
 }
 </script>
